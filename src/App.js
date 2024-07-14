6c4753c1f9de0ef01afac410a82158f3f1ec317d
siddhantekale <siddhant.ekale1994@gmail.com>
2024-07-14 12:02:23 -0700

    launchlist integrated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Typography, TextField, Button, Box } from '@mui/material';
 
 function App() {
+  // Function to dynamically load the external script
+  const loadScript = (src) => {
+    // Check if the script is already loaded
+    if (document.querySelector(`script[src="${src}"]`)) {
+      return Promise.resolve();
+    }
+
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = src;
+      script.defer = true;
+      script.onload = () => resolve(script);
+      script.onerror = () => reject(new Error(`Failed to load script ${src}`));
+      document.body.appendChild(script);
+    });
+  };
+
+  // Use useEffect to load the script when the component mounts
+  useEffect(() => {
+    loadScript('https://getlaunchlist.com/js/widget.js')
+      .then(() => {
+        console.log('External script loaded');
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <div
       style={{
@@ -23,6 +51,7 @@ function App() {
           borderRadius: '10px',
         }}
       >
+        
         <img src="/assets/logo.png" alt="Logo" style={{ width: '150px', marginBottom: '20px' }} />
         {/* <Typography variant="h2" gutterBottom>
           Welcome to Our Website!
@@ -38,16 +67,13 @@ function App() {
           noValidate
           autoComplete="off"
         >
-          <TextField
-            fullWidth
-            label="Email"
-            variant="outlined"
-            type="email"
-            required
-          />
-          <Button variant="contained" color="primary" fullWidth>
-            Register
-          </Button>
+          {/* Adding the Launchlist Widget */}
+          <div
+            className="launchlist-widget"
+            data-key-id="ocdNxw"
+            data-height="180px"
+            style={{ marginTop: '20px' }}
+          ></div>
         </Box>
       </Container>
     </div>
